ed615706860203c1a7afbcc202f9cdbbafb78925
Higinio <higiniopaternaortiz@gmail.com>
2026-08-31 09:53:00 +0200

    feat: add remote Terraform state backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.15.0, < 2.0.0"
 
+  backend "s3" {
+    bucket       = "ecb-daily-exchange-rates-tfstate-065451207233"
+    key          = "aws-daily-exchange-rates/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
